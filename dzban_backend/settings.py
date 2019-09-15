d0d4a9f935ebe92a11696fdc2ee53cbaeb0fe5fc
Daniel Jurczak <danieljurczak@integree.eu>
2019-09-15 10:15:23 +0200

    fix

--- dzban_backend/settings.py
+++ dzban_backend/settings.py
@@ -43,11 +43,16 @@ INSTALLED_APPS = [
     'django_filters',
 
     'translation',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +137,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
+CORS_ORIGIN_WHITELIST = (
+   'https://hackapi.vuewordpress.io',
+
+)
+CORS_ORIGIN_ALLOW_ALL = True
