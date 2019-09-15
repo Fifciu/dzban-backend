a0ac16b7303796375a3898919233df7a8731d384
Daniel Jurczak <danieljurczak@integree.eu>
2019-09-15 08:54:26 +0200

    get-word

--- translation/api_views.py
+++ translation/api_views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from . import models
 from . import serializers
@@ -9,6 +12,25 @@ class TranslationViewset(viewsets.ModelViewSet):
     serializer_class = serializers.TranslationSerializer
     filterset_fields = ('word', 'id',)
 
+    @action(methods=["get"], detail=False, url_path='get-words')
+    def get_words(self, request, *args, **kwargs):
+        words = request.data['words']
+        words = list(words)
+        result = []
+        for word in words:
+            try:
+                word = models.Translation.filter(word=word).first()
+            except:
+                word = None
+            if not word:
+                result.append({"word":word.word, "meaning": word.meaning, "example": word.example})
+
+        return Response(result, HTTP_200_OK)
+
+
+
+
 class TranslationListView(ListAPIView):
     serializer_class = serializers.TranslationListSerializer
     queryset = models.Translation.objects.all()
+
